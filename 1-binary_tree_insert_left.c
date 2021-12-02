9ba77db1c4188266ebc6b199fb6c8b1702cdeffc
Timalex1 <timilehinvincent@gmail.com>
2021-12-02 09:49:51 -0800

    adding 2nd file

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -9,24 +9,24 @@
 
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
-  binary_tree_t *new = NULL;
+	binary_tree_t *new = NULL;
 
-  if (!parent)
-    return (NULL);
+	if (!parent)
+		return (NULL);
 
-  new = malloc(sizeof(binary_tree_t));
-  if (!new)
-    return (NULL);
+	new = malloc(sizeof(binary_tree_t));
+	if (!new)
+		return (NULL);
 
-  new->n = value;
-  new->parent = parent;
-  new->left = NULL;
-  new->right = NULL;
-  if (parent->left)
-  {
-    new->left = parent->left;
-    parent->left->parent = new;
-  }
-  parent->left = new;
-  return (new);
+	new->n = value;
+	new->parent = parent;
+	new->left = NULL;
+	new->right = NULL;
+	if (parent->left)
+	{
+		new->left = parent->left;
+		parent->left->parent = new;
+	}
+	parent->left = new;
+	return (new);
 }
