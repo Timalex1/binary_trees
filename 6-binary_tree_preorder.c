411d79308be9a9ff1dd59adc7a57340ea324f540
Timalex1 <timilehinvincent@gmail.com>
2021-12-02 11:24:40 -0800

    fixing file 6 bug

--- 6-binary_tree_preorder.c
+++ 6-binary_tree_preorder.c
@@ -7,14 +7,14 @@
  *
  * Return: Nothing
  */
-void binary_tree_preloader(const binary_tree_t *tree, void (*func)(int))
+void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int))
 {
 	if (!tree || !func)
 		return;
 
 	(func)(tree->n);
 	if (tree->left)
-	        binary_tree_preorder(tree->left, func);
+		binary_tree_preorder(tree->left, func);
 	if (tree->right)
 		binary_tree_preorder(tree->right, func);
 }
